4bb0a1583c3da021c36436c25a668b187f497df6
Kalle Møller <github.com@k-moeller.dk>
2015-03-04 09:59:37 +0100

    Update of travis [no ci]

--- .travis-Projektrapport.sh
+++ .travis-Projektrapport.sh
@@ -1,32 +1,28 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  git clone https://${GH_TOKEN}@github.com/KalleDK/KTIB_Rapport > /dev/null 2>&1	
-  cd KTIB_Rapport/
-  git rm -rf Latex
-  git rm -rf Projektrapporten
-  cp -r ../Projektrapporten .
-  cp -r ../Latex .
+  # Fix inputs da vi flytter mapperne
+  find Latex/ -name *.tex -exec sed -i.bak s/..\\/Latex\\///g {} \;
+  sed -i.bak s/..\\/Latex\\///g Projektdokumentation/Main.tex
+  sed -i.bak s/..\\/Latex\\///g Projektrapport/Main.tex
+  find . -name *.bak -delete
+  
+  cd $TRAVIS_BUILD_DIR
+  git clone https://${GH_TOKEN}@github.com/KalleDK/KTIB_Rapport > /dev/null 2>&1
+  cd KTIB_Rapport
+  git rm -rf *
+  cp -r ../Projektrapport/* .
+  cp -r ../Latex/* .
+  cp -r ../.latex.yml .
   git add -f .
   git commit -m "Travis CI"
   git push origin master
   
   cd $TRAVIS_BUILD_DIR
-  
-  mkdir sharetex_dokumentation
-  cd sharetex_dokumentation
-  git clone https://${GH_TOKEN}@github.com/KalleDK/KTIB_Dokumentation.git . > /dev/null 2>&1
+  git clone https://${GH_TOKEN}@github.com/KalleDK/KTIB_Dokumentation > /dev/null 2>&1
+  cd KTIB_Dokumentation
   git rm -rf *
-  cd ..
-
-  find Latex/ -name *.tex -exec sed -i.bak s/..\\/Latex\\///g {} \;
-  sed -i.bak s/..\\/Latex\\///g Projektdokumentation/Main.tex
-
-  cp -r Projektdokumentation/* sharetex_dokumentation/
-  cp -r Latex/* sharetex_dokumentation/
-
-  cd sharetex_dokumentation
-  find . -name *.bak -delete
-  echo "compiler: pdflatex" > .latex.yml
-  echo "root_file : Main.tex" >> .latex.yml
+  cp -r ../Projektdokumentation/* .
+  cp -r ../Latex/* .
+  cp -r ../.latex.yml .
   git add -f .
   git commit -m "Travis CI"
   git push origin master
