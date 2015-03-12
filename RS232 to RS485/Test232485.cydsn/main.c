5fa47003fc6a37985a23140c7154ee8626354182
KasperSK <raizzat@gmail.com>
2015-03-12 10:48:39 +0100

    Test

--- RS232 to RS485/Test232485.cydsn/main.c
+++ RS232 to RS485/Test232485.cydsn/main.c
@@ -24,8 +24,16 @@ int main()
         /* Place your application code here. */
         if(!Pin_PB_Read())
         {
-//            while(!Pin_PB_Read());
+            while(!Pin_PB_Read());
             UART_PutChar('1');
+            UART_PutChar('2');
+            UART_PutChar('3');
+            UART_PutChar('4');
+            UART_PutChar('5');
+            UART_PutChar('6');
+            UART_PutChar('7');
+            UART_PutChar('8');
+            UART_PutChar('9');
         }
     }
 }
